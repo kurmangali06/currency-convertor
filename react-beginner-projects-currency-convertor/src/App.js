325b2dce165609ce0c8ce37d2b85d409c91cbc26
Курмангали <kurmangali0606@gmail.com>
2022-08-20 14:28:43 +0600

    change work

--- react-beginner-projects-currency-convertor/src/App.js
+++ react-beginner-projects-currency-convertor/src/App.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Block } from './Block';
 import './index.scss';
 
 function App() {
+  const [fromCurrency, setFromCurrency] = useState("KZT")
+  const [toCurrency, setToCurrency] = useState("USD")
+  const [fromPrice, setFromPrice] = useState(0)
+  const [toPrice, setToPrice] = useState(1)
+
+  const ratesRef = useRef({})
+
+  useEffect(() => {
+    fetch('https://cdn.cur.su/api/nbkz.json').then((res) => res.json()).then((json) => {
+      ratesRef.current = json.rates
+      onChangeToPrice(1)
+    }).catch((err) => {
+      console.warn(err);
+      alert("Не удалось получить информацию!")
+    })
+
+  }, [])
+
+  const onChangeFromPrice = (value) => {
+    const price = value /ratesRef.current[fromCurrency]
+    const result = price * ratesRef.current[toCurrency]
+    setFromPrice(value)
+    setToPrice(result)
+  }
+
+  const onChangeToPrice = (value) => {
+    const result = ratesRef.current[fromCurrency] / ratesRef.current[toCurrency] * value;
+    setFromPrice(result)
+    setToPrice(value)
+  }
+
+  useEffect(()=> {
+    onChangeFromPrice(fromPrice) 
+  }, [fromCurrency])
+
+  useEffect(()=> {
+    onChangeToPrice(toPrice) 
+  }, [toCurrency])
+
+
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
+      <Block value={fromPrice} currency={fromCurrency} onChangeCurrency={setFromCurrency} onChangeValue={onChangeFromPrice } />
+      <Block value={toPrice} currency={toCurrency} onChangeCurrency={setToCurrency} onChangeValue={onChangeToPrice }/>
     </div>
   );
 }
